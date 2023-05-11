1d96bfa71983216d2bcbb85bc0dd7c966d958280
Mateusz R <mateusz.rzonca@nordcloud.com>
2023-05-11 18:03:56 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,7 @@ module "s3-bucket" {
   source  = "app.terraform.io/mat-training-nordcloud/s3-bucket/aws"
   version = "2.8.0"
 
-  bucket = "my-s3-bucket"
+  bucket = "mateusz-rzonca-bucket123"
   acl    = "private"
 
   versioning = {
